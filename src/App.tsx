48efa0004b2563466cde252e6a64444b4bb4ca09
joon <sargadil@gmail.com>
2024-06-16 13:59:11 +0900

    finished the clone cache data

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,18 @@
 // import { Todo } from "./components/Todo";
-import { ProjectComponent } from "./components/Projects";
-import { TodoMutationComponent } from "./components/TodoMutation";
-import { TodoList } from "./components/TodoQueries";
+// import { ProjectComponent } from "./components/Projects";
+// import { TodoMutationComponent } from "./components/TodoMutation";
+// import { TodoList } from "./components/TodoQueries";
+
+import { ProductsComponent } from "./components/Products";
 
 function App() {
   return (
     <>
       {/* <Todo /> */}
-      <TodoMutationComponent />
-      <TodoList />
-      <ProjectComponent />
+      {/* <TodoMutationComponent /> */}
+      {/* <TodoList /> */}
+      {/* <ProjectComponent /> */}
+      <ProductsComponent />
     </>
   );
   // const [data, setData] = useState([]);
--- src/service/queries.ts
+++ src/service/queries.ts
@@ -1,5 +1,18 @@
-import { keepPreviousData, useQueries, useQuery } from "@tanstack/react-query";
-import { getProjects, getTodo, getTodosIds } from "./api";
+import {
+  keepPreviousData,
+  useInfiniteQuery,
+  useQueries,
+  useQuery,
+  useQueryClient,
+} from "@tanstack/react-query";
+import {
+  getProduct,
+  getProducts,
+  getProjects,
+  getTodo,
+  getTodosIds,
+} from "./api";
+import { Product } from "../types/product";
 
 export function useTodosIds() {
   return useQuery({
@@ -30,11 +43,58 @@ export function useTodos(ids: (number | undefined)[] | undefined) {
 }
 
 // 프로젝트 불러오기
+// 페이지네이션에 대해 알아보기
+/**
+ * - placeholderData는 이전 데이터를 캐시해두어 다음에 데이터를 불러올 때 빠르게 렌더링할 수 있게 합니다.
+ *
+ *
+ */
 export function useProjects(page: number) {
   return useQuery({
     queryKey: ["projects", { page }],
     queryFn: () => getProjects(page),
     // placeholderData: keepPreviousData,
-    placeholderData: keepPreviousData
+    placeholderData: keepPreviousData,
+  });
+}
+
+// 프로덕트들 모두 불러오기
+export function useProducts() {
+  return useInfiniteQuery({
+    queryKey: ["products"],
+    queryFn: getProducts,
+    initialPageParam: 0,
+    getNextPageParam: (lastPage, _, lastPagePram) => {
+      if (lastPage.length === 0) {
+        return undefined;
+      }
+      return lastPagePram + 1;
+    },
+    getPreviousPageParam: (_, __, firstPageParam) => {
+      if (firstPageParam <= 1) {
+        return undefined;
+      }
+      return firstPageParam - 1;
+    },
+  });
+}
+
+// 특정 프로덕트 불러오기
+export function useProduct(id: number | null) {
+  const queryClient = useQueryClient();
+  return useQuery({
+    queryKey: ["product", { id }],
+    queryFn: () => getProduct(id!),
+    enabled: !!id,
+    placeholderData: () => {
+      const cachedProducts = (
+        queryClient.getQueryData(["products"]) as {
+          pages: Product[] | undefined;
+        }
+      )?.pages?.flat(2);
+      if (cachedProducts) {
+        return cachedProducts.find((item) => item.id === id);
+      }
+    },
   });
 }
